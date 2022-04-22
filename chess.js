03048e8fb2077ac969afe6885a29a0573e5f9f46
Matan Haimov <shlomihaimov1@gmail.com>
2022-04-23 01:04:12 +0300

    first commit

---
+++ chess.js
@@ -0,0 +1,19 @@
+import empty from './empty.js';
+import king from './king.js';
+
+
+function chessboard() {
+    const game_over = false;
+    const locations = ["Saab", "Volvo", "BMW"];
+
+    while (!game_over) {
+        for (let i = 1; i <= 8; i++) {
+            for (let j = 1; j <= 8; j++) {
+                
+            }
+        }
+        
+    }
+
+    return render
+}
